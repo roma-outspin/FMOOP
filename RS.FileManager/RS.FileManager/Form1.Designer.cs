d0cfa5aff65568311348e4c6babe9cffc7d15a71
roma_outspin <roma_outspin@mail.ru>
2022-01-04 15:10:07 +0300

    first steps
    -disk list
    -ierarchy list

--- RS.FileManager/RS.FileManager/Form1.Designer.cs
+++ RS.FileManager/RS.FileManager/Form1.Designer.cs
@@ -29,13 +29,95 @@ namespace RS.FileManager
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.FMTree = new System.Windows.Forms.TreeView();
+            this.CopyBtn = new System.Windows.Forms.Button();
+            this.MoveBtn = new System.Windows.Forms.Button();
+            this.DeleteBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.UpdateDisksBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FMTree
+            // 
+            this.FMTree.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FMTree.Location = new System.Drawing.Point(12, 74);
+            this.FMTree.Name = "FMTree";
+            this.FMTree.Size = new System.Drawing.Size(776, 401);
+            this.FMTree.TabIndex = 0;
+            // 
+            // CopyBtn
+            // 
+            this.CopyBtn.Location = new System.Drawing.Point(12, 489);
+            this.CopyBtn.Name = "CopyBtn";
+            this.CopyBtn.Size = new System.Drawing.Size(89, 23);
+            this.CopyBtn.TabIndex = 1;
+            this.CopyBtn.Text = "Копировать";
+            this.CopyBtn.UseVisualStyleBackColor = true;
+            this.CopyBtn.Click += new System.EventHandler(this.CopyBtn_Click);
+            // 
+            // MoveBtn
+            // 
+            this.MoveBtn.Location = new System.Drawing.Point(107, 489);
+            this.MoveBtn.Name = "MoveBtn";
+            this.MoveBtn.Size = new System.Drawing.Size(89, 23);
+            this.MoveBtn.TabIndex = 2;
+            this.MoveBtn.Text = "Переместить";
+            this.MoveBtn.UseVisualStyleBackColor = true;
+            // 
+            // DeleteBtn
+            // 
+            this.DeleteBtn.Location = new System.Drawing.Point(202, 489);
+            this.DeleteBtn.Name = "DeleteBtn";
+            this.DeleteBtn.Size = new System.Drawing.Size(89, 23);
+            this.DeleteBtn.TabIndex = 3;
+            this.DeleteBtn.Text = "Удалить";
+            this.DeleteBtn.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 23);
+            this.textBox1.TabIndex = 4;
+            // 
+            // UpdateDisksBtn
+            // 
+            this.UpdateDisksBtn.Location = new System.Drawing.Point(615, 12);
+            this.UpdateDisksBtn.Name = "UpdateDisksBtn";
+            this.UpdateDisksBtn.Size = new System.Drawing.Size(173, 23);
+            this.UpdateDisksBtn.TabIndex = 5;
+            this.UpdateDisksBtn.Text = "Обновить список дисков";
+            this.UpdateDisksBtn.UseVisualStyleBackColor = true;
+            this.UpdateDisksBtn.Click += new System.EventHandler(this.UpdateDisksBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 526);
+            this.Controls.Add(this.UpdateDisksBtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.DeleteBtn);
+            this.Controls.Add(this.MoveBtn);
+            this.Controls.Add(this.CopyBtn);
+            this.Controls.Add(this.FMTree);
+            this.Name = "Form1";
+            this.Text = "Мой файловый менеджер";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView FMTree;
+        private System.Windows.Forms.Button CopyBtn;
+        private System.Windows.Forms.Button MoveBtn;
+        private System.Windows.Forms.Button DeleteBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button UpdateDisksBtn;
     }
 }
 
--- RS.FileManager/RS.FileManager/Form1.cs
+++ RS.FileManager/RS.FileManager/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,102 @@ namespace RS.FileManager
 {
     public partial class Form1 : Form
     {
+        private List<string> _disks;
+        private List<Button> _disksBtn;
+
         public Form1()
         {
             InitializeComponent();
+            _disksBtn = new List<Button>();
+            UpdateDisks();
+            //UpdateFileIerarchy(_disks.FirstOrDefault());
         }
 
+
+        private void CopyBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UpdateDisks()
+        {
+            if(_disksBtn.Count>0)
+            {
+                for(int i = 0; i < _disksBtn.Count;i++)
+                {
+                    DeleteDiskButton(_disksBtn[i]);
+                }
+            }
+
+            _disksBtn.Clear();
+
+            _disks = GetDisks().ToList();
+
+            for (int i = 0; i < _disks.Count; i++)
+            {
+                AddDiskButton(_disks[i], i);
+            }
+
+        }
+
+        private IEnumerable<string> GetDisks()
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            foreach(var drive in allDrives)
+            {
+                yield return drive.Name;
+            }
+        }
+
+        private void AddDiskButton(string name, int number)
+        {
+            int btnWidth = 80;
+            var result = new Button();
+            result.Name = "myDISKBTN" + number;
+            result.Text = name;
+            result.Click += Result_Click;
+
+            Point position = new Point();
+            position.X = 12 + number * btnWidth;
+            position.Y = 10;
+            result.Location=position;
+
+            _disksBtn.Add(result);
+            Controls.Add(result);
+        }
+
+        private void Result_Click(object sender, EventArgs e)
+        {
+            UpdateFileIerarchy(((Button)sender).Text);
+        }
+
+        private void DeleteDiskButton(Button diskBtn)
+        {
+            if (Controls.Contains(diskBtn))
+            {
+                Controls.Remove(diskBtn);
+            }  
+        }
+
+        private void UpdateFileIerarchy(string rootPath)
+        {
+            FMTree.Nodes.Clear();
+            var entries = Directory.GetFileSystemEntries(rootPath);
+
+
+            FMTree.Nodes.Add(rootPath);
+
+            foreach (var entry in entries)
+            {
+                FMTree.Nodes[0].Nodes.Add(entry.Substring(rootPath.Length-1));
+            }
+           
+        }
+
+        private void UpdateDisksBtn_Click(object sender, EventArgs e)
+        {
+            UpdateDisks();
+        }
     }
 }
