994571a24b22e267fe1abea7249c366afa5a8916
roma_outspin <roma_outspin@mail.ru>
2022-01-25 19:56:49 +0300

    Revert "ListView"
    This reverts commit 1055807567262cce17b10c8a5a0dd19447d9ef41.

--- RS.FileManager/RS.FileManager/Form1.Designer.cs
+++ RS.FileManager/RS.FileManager/Form1.Designer.cs
@@ -29,19 +29,12 @@ namespace RS.FileManager
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.FMTree = new System.Windows.Forms.TreeView();
             this.CopyBtn = new System.Windows.Forms.Button();
             this.MoveBtn = new System.Windows.Forms.Button();
             this.DeleteBtn = new System.Windows.Forms.Button();
-            this.searchMask = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.UpdateDisksBtn = new System.Windows.Forms.Button();
-            this.searchResult = new System.Windows.Forms.ListView();
-            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
-            this.FMListView = new System.Windows.Forms.ListView();
-            this.iconsList = new System.Windows.Forms.ImageList(this.components);
-            this.textBoxPath = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
             this.SuspendLayout();
             // 
             // FMTree
@@ -49,11 +42,10 @@ namespace RS.FileManager
             this.FMTree.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.FMTree.Location = new System.Drawing.Point(1154, 145);
+            this.FMTree.Location = new System.Drawing.Point(12, 74);
             this.FMTree.Name = "FMTree";
-            this.FMTree.Size = new System.Drawing.Size(89, 53);
+            this.FMTree.Size = new System.Drawing.Size(776, 401);
             this.FMTree.TabIndex = 0;
-            this.FMTree.DoubleClick += new System.EventHandler(this.FMTree_DoubleClick);
             // 
             // CopyBtn
             // 
@@ -83,17 +75,16 @@ namespace RS.FileManager
             this.DeleteBtn.Text = "Удалить";
             this.DeleteBtn.UseVisualStyleBackColor = true;
             // 
-            // searchMask
+            // textBox1
             // 
-            this.searchMask.Location = new System.Drawing.Point(761, 45);
-            this.searchMask.Name = "searchMask";
-            this.searchMask.Size = new System.Drawing.Size(184, 23);
-            this.searchMask.TabIndex = 4;
-            this.searchMask.KeyUp += new System.Windows.Forms.KeyEventHandler(this.searchMask_KeyUp);
+            this.textBox1.Location = new System.Drawing.Point(12, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 23);
+            this.textBox1.TabIndex = 4;
             // 
             // UpdateDisksBtn
             // 
-            this.UpdateDisksBtn.Location = new System.Drawing.Point(578, 12);
+            this.UpdateDisksBtn.Location = new System.Drawing.Point(615, 12);
             this.UpdateDisksBtn.Name = "UpdateDisksBtn";
             this.UpdateDisksBtn.Size = new System.Drawing.Size(173, 23);
             this.UpdateDisksBtn.TabIndex = 5;
@@ -101,65 +92,19 @@ namespace RS.FileManager
             this.UpdateDisksBtn.UseVisualStyleBackColor = true;
             this.UpdateDisksBtn.Click += new System.EventHandler(this.UpdateDisksBtn_Click);
             // 
-            // searchResult
-            // 
-            this.searchResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.searchResult.HideSelection = false;
-            this.searchResult.Location = new System.Drawing.Point(1154, 45);
-            this.searchResult.Name = "searchResult";
-            this.searchResult.Size = new System.Drawing.Size(103, 89);
-            this.searchResult.TabIndex = 6;
-            this.searchResult.UseCompatibleStateImageBehavior = false;
-            this.searchResult.View = System.Windows.Forms.View.List;
-            // 
-            // fileSystemWatcher1
-            // 
-            this.fileSystemWatcher1.EnableRaisingEvents = true;
-            this.fileSystemWatcher1.SynchronizingObject = this;
-            // 
-            // FMListView
-            // 
-            this.FMListView.HideSelection = false;
-            this.FMListView.Location = new System.Drawing.Point(12, 84);
-            this.FMListView.MultiSelect = false;
-            this.FMListView.Name = "FMListView";
-            this.FMListView.Size = new System.Drawing.Size(933, 399);
-            this.FMListView.TabIndex = 7;
-            this.FMListView.UseCompatibleStateImageBehavior = false;
-            // 
-            // iconsList
-            // 
-            this.iconsList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
-            this.iconsList.ImageSize = new System.Drawing.Size(32, 32);
-            this.iconsList.TransparentColor = System.Drawing.Color.Transparent;
-            // 
-            // textBoxPath
-            // 
-            this.textBoxPath.Location = new System.Drawing.Point(12, 45);
-            this.textBoxPath.Name = "textBoxPath";
-            this.textBoxPath.Size = new System.Drawing.Size(559, 23);
-            this.textBoxPath.TabIndex = 8;
-            this.textBoxPath.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxPath_KeyDown);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1310, 526);
-            this.Controls.Add(this.textBoxPath);
-            this.Controls.Add(this.FMListView);
-            this.Controls.Add(this.searchResult);
+            this.ClientSize = new System.Drawing.Size(800, 526);
             this.Controls.Add(this.UpdateDisksBtn);
-            this.Controls.Add(this.searchMask);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.DeleteBtn);
             this.Controls.Add(this.MoveBtn);
             this.Controls.Add(this.CopyBtn);
             this.Controls.Add(this.FMTree);
             this.Name = "Form1";
             this.Text = "Мой файловый менеджер";
-            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -171,13 +116,8 @@ namespace RS.FileManager
         private System.Windows.Forms.Button CopyBtn;
         private System.Windows.Forms.Button MoveBtn;
         private System.Windows.Forms.Button DeleteBtn;
-        private System.Windows.Forms.TextBox searchMask;
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button UpdateDisksBtn;
-        private System.Windows.Forms.ListView searchResult;
-        private System.IO.FileSystemWatcher fileSystemWatcher1;
-        private System.Windows.Forms.ListView FMListView;
-        private System.Windows.Forms.ImageList iconsList;
-        private System.Windows.Forms.TextBox textBoxPath;
     }
 }
 
--- RS.FileManager/RS.FileManager/Form1.cs
+++ RS.FileManager/RS.FileManager/Form1.cs
@@ -15,41 +15,19 @@ namespace RS.FileManager
     {
         private List<string> _disks;
         private List<Button> _disksBtn;
-        private MyFileManager _fm;
-        private ImageList imgs;
 
         public Form1()
         {
             InitializeComponent();
-            imgs = new ImageList();
-            imgs.ImageSize = new Size(32, 32);
-            FMListView.LargeImageList = imgs;
-            Icon folderIcon = IconExtractor.Extract(@"%SYSTEMROOT%\System32\Shell32.dll",4,true);
-            imgs.Images.Add(folderIcon);
-
             _disksBtn = new List<Button>();
             UpdateDisks();
-            _fm = new MyFileManager(_disks.FirstOrDefault());
-            UpdateFileIerarchy();
-
+            //UpdateFileIerarchy(_disks.FirstOrDefault());
         }
-
-
 
 
         private void CopyBtn_Click(object sender, EventArgs e)
         {
-            IFMEntity entity;
-            string selected = FMTree.SelectedNode.Text;
-            if (File.Exists(_fm.Path+selected))
-            {
-                entity = new FMFile(_fm.Path, selected);
-            } else
-            {
-                entity = new FMFolder(_fm.Path, selected);
-            }
 
-            _fm.Execute(new FMCopyCommand(entity, _fm.Path, "Копия " + selected));
         }
 
         private void UpdateDisks()
@@ -102,8 +80,7 @@ namespace RS.FileManager
 
         private void Result_Click(object sender, EventArgs e)
         {
-            _fm.Path = ((Button)sender).Text;
-            UpdateFileIerarchy();
+            UpdateFileIerarchy(((Button)sender).Text);
         }
 
         private void DeleteDiskButton(Button diskBtn)
@@ -114,77 +91,24 @@ namespace RS.FileManager
             }  
         }
 
-        private void UpdateFileIerarchy()
+        private void UpdateFileIerarchy(string rootPath)
         {
             FMTree.Nodes.Clear();
-            FMListView.Items.Clear();
-            textBoxPath.Text = _fm.Path;
-            FMTree.Nodes.Add(_fm.Path);
-            foreach (var entity in _fm.FilesAndFolders)
+            var entries = Directory.GetFileSystemEntries(rootPath);
+
+
+            FMTree.Nodes.Add(rootPath);
+
+            foreach (var entry in entries)
             {
-                FMTree.Nodes[0].Nodes.Add(entity.Name);
-                if(entity is FMFile)
-                {
-                    imgs.Images.Add(System.Drawing.Icon.ExtractAssociatedIcon(entity.Path));
-                    FMListView.Items.Add(entity.Name, imgs.Images.Count - 1);
-                } else
-                {
-                    FMListView.Items.Add(entity.Name, 0);
-                }
-                
-                
-
+                FMTree.Nodes[0].Nodes.Add(entry.Substring(rootPath.Length-1));
             }
+           
         }
-
 
         private void UpdateDisksBtn_Click(object sender, EventArgs e)
         {
             UpdateDisks();
-        }
-
-        private void searchMask_KeyUp(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Enter)
-            {
-                
-                var test = _fm.Search(_fm.Path, searchMask.Text);
-                foreach(var item in test)
-                {
-                    searchResult.Items.Add(new ListViewItem(item));
-                }
-            }
-        }
-
-        private void FMTree_DoubleClick(object sender, EventArgs e)
-        {
-            var clickedPath = FMTree.Nodes[0].Text + FMTree.SelectedNode.Text+"\\";
-            if (Directory.Exists(clickedPath))
-            {
-                _fm = new MyFileManager(clickedPath);
-                UpdateFileIerarchy();
-            }
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
-        private void textBoxPath_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Enter)
-            {
-                if (Directory.Exists(textBoxPath.Text))
-                {
-                    _fm.Path = textBoxPath.Text;
-                    UpdateFileIerarchy();
-                }else
-                {
-                    MessageBox.Show("Путь не найден");
-                }
-                
-            }
         }
     }
 }
